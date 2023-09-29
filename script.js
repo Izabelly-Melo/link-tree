f412c7f05462a882a510b58d3078809bbfcd8bd9
Izabelly <98113162+Izabelly-Melo@users.noreply.github.com>
2023-09-28 22:10:31 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode () {
+    const html = document.body
+
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', './assets/itadori.png')
+        img.setAttribute('alt', 'itadori')
+    } else {
+        img.setAttribute('src', './assets/sukuna.png')
+        img.setAttribute('alt', 'sukuna')
+    }
+
+}
